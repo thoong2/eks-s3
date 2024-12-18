49bd7af477e70ede08b6c70308e7033db8f94845
thoong2 <acc.thongnguyen98.84@gmail.com>
2024-12-18 10:38:11 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,6 +30,36 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
+# Tạo IAM Role cho Node Group
+resource "aws_iam_role" "node_group_role" {
+  name = "eks-node-group-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Principal = { Service = "ec2.amazonaws.com" },
+      Action    = "sts:AssumeRole",
+    }]
+  })
+}
+
+# Gán các policy cần thiết cho Node Group
+resource "aws_iam_role_policy_attachment" "node_group_policy" {
+  role       = aws_iam_role.node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "cni_policy" {
+  role       = aws_iam_role.node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_policy" {
+  role       = aws_iam_role.node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
 # Tạo EKS Cluster
 resource "aws_eks_cluster" "eks_cluster" {
   name     = var.cluster_name
@@ -46,7 +76,7 @@ resource "aws_eks_cluster" "eks_cluster" {
 resource "aws_eks_node_group" "node_group" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = "eks-node-group"
-  node_role_arn   = aws_iam_role.eks_role.arn
+  node_role_arn   = aws_iam_role.node_group_role.arn  # Sử dụng role mới
 
   scaling_config {
     desired_size = var.desired_capacity
@@ -56,5 +86,5 @@ resource "aws_eks_node_group" "node_group" {
 
   instance_types = [var.node_instance_type]
 
-  subnet_ids = data.aws_subnets.default.ids  # Cung cấp subnet cho node group
+  subnet_ids = data.aws_subnets.default.ids
 }
