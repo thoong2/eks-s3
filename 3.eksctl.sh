336b5c6140463cc6f3442804865aabbe96ddc221
thoong2 <acc.thongnguyen98.84@gmail.com>
2024-12-18 10:38:49 +0000

    Update 3.eksctl.sh

--- 3.eksctl.sh
+++ 3.eksctl.sh
@@ -1,3 +1,6 @@
+eksctl utils associate-iam-oidc-provider --region=ap-southeast-1 --cluster=eks-demo-cluster --approve
+
+
 eksctl create iamserviceaccount \
   --name s3-access-sa \
   --namespace default \
